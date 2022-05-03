f0efae4b3f6539f8082593eff1d1f0a8522ecd47
Sascha Slabschi <sascha.slabschi@codeflow.biz>
2022-05-03 15:27:47 +0200

    built with upstream changes.

--- public/assets/scripts/choices.js
+++ public/assets/scripts/choices.js
@@ -4641,7 +4641,7 @@ var templates = {
     div.setAttribute('aria-expanded', 'false');
 
     if (labelId) {
-      div.setAttribute('aria-labeledby', labelId);
+      div.setAttribute('aria-labelledby', labelId);
     }
 
     return div;
